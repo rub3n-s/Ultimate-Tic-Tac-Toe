8ee683ead951218695321c212f3b62ed6f9a90a8
Rúben Santos <54284863+rub3n-s@users.noreply.github.com>
2023-04-22 19:18:55 +0100

    nav&footer

---
+++ ultimate-tic-tac-toe/src/components/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Ultimate Tic-Tac-Toe ©</p>
+            <p>Practical work made by:</p>
+            <p>&emsp;&emsp;Rúben Mendes Santos</p>
+            <p>&emsp;&emsp;2019116244</p>
+            <p><em>Linguagens Script @ DEIS</em></p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ ultimate-tic-tac-toe/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import './Navbar.css';
+import {useRef} from 'react';
+
+/* Navigation Bar Component */
+const Navbar = () => {
+    const nav = useRef(null);
+    const showHide = () =>  nav.current.classList.toggle('show');
+    return (
+      <header>
+        <div className='wrapper'>
+          <div className='logo'>
+            <a href="index.html"><img src='logo.png' alt='Ultimate Tic-Tac-Toe'/></a>
+          </div>
+          <div className='navbar' ref={nav}>
+            <div className='close-nav'><button onClick={showHide}>×</button></div>
+            <nav>
+              <ul>
+                  <li><a href='index.html'>Inicio</a></li>
+                  <li><a href='#'>Top10</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div className='menu-bar'>
+              <button onClick={showHide}><i></i></button>
+          </div>
+        </div>
+      </header>
+    );
+}
+
+export default Navbar;
