a1d4cbab946d09453880d2023c44c7043142e0be
Rúben Santos <54284863+rub3n-s@users.noreply.github.com>
2023-04-24 18:34:27 +0100

    cell click

--- ultimate-tic-tac-toe/src/components/GamePanel.js
+++ ultimate-tic-tac-toe/src/components/GamePanel.js
@@ -2,46 +2,39 @@ import "./GamePanel.css";
 
 const buildGrids = () => {
   let content = [];
-  let outerGrid = 9;  // 3x3 grid
+  let outerGrid = 9; // 3x3 grid
   for (let i = 0; i < outerGrid; i++) {
-    content.push(
-      <div className="box">
-        {gameGrid()}
-      </div>
-    );
+    content.push(gameGrid(i));
   }
   return content;
 };
 
-const gameGrid = () => {
+const gameGrid = (i) => {
+  // This function receives the grid index
+  // Each cell has a click event, where the cell index is sent
+  // to a separate function to handle click events
   return (
-    // TODO
-    //criar listeners para o click de cada celula
-    
     <div className="box">
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
+      <div className="cell" onClick={() => clickHandle(this, i, 1, 1)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 1, 2)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 1, 3)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 2, 1)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 2, 2)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 2, 3)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 3, 1)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 3, 2)}></div>
+      <div className="cell" onClick={() => clickHandle(i, 3, 3)}></div>
     </div>
   );
 };
 
+const clickHandle = (element, grid, line, column) => {
+  element.classList.add("clicked");
+  console.log(`[Cell Click] grid:${grid}, cell(${line},${column})`);
+};
+
 const GamePanel = ({ showGrid }) => {
-  return (
-    <>
-      {showGrid && (
-        <div className="grid-wrapper">
-          {buildGrids()}
-        </div>
-      )}
-    </>
-  );
+  return <>{showGrid && <div className="grid-wrapper">{buildGrids()}</div>}</>;
 };
 
 export default GamePanel;
